674e0e5e69ffeed19d9a442154be6e7e6ef39437
amr masoud <amr.masoud68@gmail.com>
2018-02-02 18:01:32 +0200

    +twitter api

--- app/src/main/java/com/amr/twitter/activities/FollowerDetailsActivity.java
+++ app/src/main/java/com/amr/twitter/activities/FollowerDetailsActivity.java
@@ -4,12 +4,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.amr.twitter.R;
+import com.amr.twitter.fragments.FollowerDetailsFragment;
+import com.twitter.sdk.android.core.models.User;
 
 public class FollowerDetailsActivity extends AppCompatActivity {
+
+    private User follower;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_follower_details);
+
+        //if ()
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.item_detail_container, FollowerDetailsFragment.newInstance(follower))
+                    .commit();
+        }
     }
 }
--- app/src/main/java/com/amr/twitter/fragments/FollowerDetailsFragment.java
+++ app/src/main/java/com/amr/twitter/fragments/FollowerDetailsFragment.java
@@ -1,53 +1,42 @@
 package com.amr.twitter.fragments;
 
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.amr.twitter.R;
+import com.bumptech.glide.Glide;
+import com.twitter.sdk.android.core.models.User;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FollowerDetailsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FollowerDetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class FollowerDetailsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    public static final String ITEM_FOLLOWER = "ITEM_FOLLOWER";
     private OnFragmentInteractionListener mListener;
+    private User follower;
+    @BindView(R.id.img_follower_profile)
+    ImageView imgFollowerProfile;
+    @BindView(R.id.tv_follower_name)
+    TextView tvFollowerName;
+    @BindView(R.id.tv_follower_hash)
+    TextView tvFollowerHash;
+    @BindView(R.id.tv_follower_bio)
+    TextView tvFollowerBio;
 
     public FollowerDetailsFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FollowerDetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FollowerDetailsFragment newInstance(String param1, String param2) {
+    public static FollowerDetailsFragment newInstance(User user) {
         FollowerDetailsFragment fragment = new FollowerDetailsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putSerializable(ITEM_FOLLOWER, user);
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,26 +45,20 @@ public class FollowerDetailsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            follower = (User) getArguments().getSerializable(ITEM_FOLLOWER);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_follower_details, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_follower_details, container, false);
+        ButterKnife.bind(this, rootView);
+        initView();
+        return rootView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
+    /*@Override
     public void onAttach(Context context) {
         super.onAttach(context);
         if (context instanceof OnFragmentInteractionListener) {
@@ -90,20 +73,16 @@ public class FollowerDetailsFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }*/
+
+    private void initView() {
+        Glide.with(imgFollowerProfile.getContext()).load(follower.profileImageUrl).into(imgFollowerProfile);
+        tvFollowerBio.setText(follower.description);
+        tvFollowerName.setText(follower.name);
+        tvFollowerHash.setText(follower.url);
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
